8987911ea9df20d5eb1429c6aec3b3bf6cf07c80
Albina Salkayeva <salkaeva.a@gmail.com>
2025-06-15 21:43:24 +0500

    Initialize project

---
+++ src/test/java/com/albina0104/url_shortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albina0104.url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
